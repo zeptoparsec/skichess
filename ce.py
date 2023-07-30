4bee0aafd8cad50920b63a0454f83684c536362b
unknown <qwern1177@outlook.com>
2023-07-30 07:13:01 +0530

    add a class for the board

--- ce.py
+++ ce.py
@@ -1,10 +1,16 @@
-board = [
-    'r','n','b','q','k','b','n','r',
-    'p','p','p','p','p','p','p','p',
-    'e','e','e','e','e','e','e','e',
-    'e','e','e','e','e','e','e','e',
-    'e','e','e','e','e','e','e','e',
-    'e','e','e','e','e','e','e','e',
-    'P','P','P','P','P','P','P','P',
-    'R','N','B','Q','K','B','N','R'
-]
+class boardstate:
+    def __init__(self) -> None:
+        self._board = [
+            'r','n','b','q','k','b','n','r',
+            'p','p','p','p','p','p','p','p',
+            'e','e','e','e','e','e','e','e',
+            'e','e','e','e','e','e','e','e',
+            'e','e','e','e','e','e','e','e',
+            'e','e','e','e','e','e','e','e',
+            'P','P','P','P','P','P','P','P',
+            'R','N','B','Q','K','B','N','R'
+        ]
+    def makemove(self,startpos, endpos):
+        tmp = self._board[startpos]
+        self._board[startpos] = self._board[endpos]
+        self._board[endpos] = tmp # Maybe do some move validation here?
