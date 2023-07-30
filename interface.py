988ec4399d1139c892f454443543a3226efa5c3f
unknown <qwern1177@outlook.com>
2023-07-30 17:39:43 +0530

    since turn can only take 2 values, it can be a bool

--- interface.py
+++ interface.py
@@ -4,15 +4,19 @@ from time import sleep
 
 board = Boardstate()
 
-turn = "w"
+turn = True
 while True:
     system('cls')
     board.printboard()
-    if turn == 'w': move = input("White's turn: ").lower()
-    else: move = input("Black's turn: ")
+    if turn:
+        move = input("White's turn: ").lower()
+    else:
+        move = input("Black's turn: ")
 
-    try: board.ucimakemove(move)
+    try:
+        board.ucimakemove(move)
     except: 
         print("Invalid move!\n")
         sleep(1)
-    else: turn = 'w' if turn == 'b' else 'b'
+    else:
+        turn = not turn
