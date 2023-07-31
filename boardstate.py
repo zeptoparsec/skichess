e05075ce9e6e620357f89244661cbca932cc89c3
unknown <qwern1177@outlook.com>
2023-07-31 16:59:28 +0530

    remove useless type alias

--- boardstate.py
+++ boardstate.py
@@ -1,35 +1,33 @@
 from piece import Piece
 from checkmove import Checkmove
 
-piece = Piece
-
 class Boardstate:
     def __init__(self):
         self._board = [None]*64
-        self._board[0] = piece('R',0,5,'B')
-        self._board[1] = piece('N',1,3.2,'B')
-        self._board[2] = piece('B',2,3.3,'B')
-        self._board[3] = piece('Q',3,9,'B')
-        self._board[4] = piece('K',4,200,'B')
-        self._board[5] = piece('B',5,3.3,'B')
-        self._board[6] = piece('N',6,3.2,'B')
-        self._board[7] = piece('R',7,5,'B')
-        self._board[63-0] = piece('R',0,5,'W')
-        self._board[63-1] = piece('N',1,3.2,'W')
-        self._board[63-2] = piece('B',2,3.3,'W')
-        self._board[63-3] = piece('K',3,200,'W')
-        self._board[63-4] = piece('Q',4,9,'W')
-        self._board[63-5] = piece('B',5,3.3,'W')
-        self._board[63-6] = piece('N',6,3.2,'W')
-        self._board[63-7] = piece('R',7,5,'W')
+        self._board[0] = Piece('R',0,5,'B')
+        self._board[1] = Piece('N',1,3.2,'B')
+        self._board[2] = Piece('B',2,3.3,'B')
+        self._board[3] = Piece('Q',3,9,'B')
+        self._board[4] = Piece('K',4,200,'B')
+        self._board[5] = Piece('B',5,3.3,'B')
+        self._board[6] = Piece('N',6,3.2,'B')
+        self._board[7] = Piece('R',7,5,'B')
+        self._board[63-0] = Piece('R',0,5,'W')
+        self._board[63-1] = Piece('N',1,3.2,'W')
+        self._board[63-2] = Piece('B',2,3.3,'W')
+        self._board[63-3] = Piece('K',3,200,'W')
+        self._board[63-4] = Piece('Q',4,9,'W')
+        self._board[63-5] = Piece('B',5,3.3,'W')
+        self._board[63-6] = Piece('N',6,3.2,'W')
+        self._board[63-7] = Piece('R',7,5,'W')
 
         for i in range(8,16):
-            self._board[i] = piece('P',i,1,'B')
+            self._board[i] = Piece('P',i,1,'B')
         for i in range(48,56):
-            self._board[i] = piece('P',i,1,'W')
+            self._board[i] = Piece('P',i,1,'W')
 
         for i in range(16,48):
-            self._board[i] = piece('E',i,0,'N')
+            self._board[i] = Piece('E',i,0,'N')
 
     def printboard(self):
         pieces = {'PW': '♟', 'RW': '♜', 'NW': '♞', 'BW': '♝', 'KW': '♚', 'QW': '♛', 'EN' : ' ',
@@ -58,7 +56,7 @@ class Boardstate:
             return -1
         elif self._board[endpos].col != 'N':
             self._board[endpos] = self._board[startpos]
-            self._board[startpos] = piece('E',startpos,0,'N')
+            self._board[startpos] = Piece('E',startpos,0,'N')
         else:
             self._board[startpos], self._board[endpos] = self._board[endpos], self._board[startpos]
         return 0
