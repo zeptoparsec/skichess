08acbf3663c983c962e54431d31517f758ad0262
Kiwi-Co <111125019+Kiwi-Co@users.noreply.github.com>
2023-08-04 15:52:16 +0530

    Update checkmove.py

--- checkmove.py
+++ checkmove.py
@@ -4,7 +4,7 @@ class Checkmove:
 
     def __check_pawn(self):
         if self.board[self.pos].col == 'B': 
-            if self.pos <= 15 and self.pos >= 8:
+            if 15 >= self.pos >= 8:
                 if self.target == self.pos + (8) or self.target == self.pos + (8*2):
                     return self.board[self.target].col == 'N'
             elif self.target == self.pos + (8) and self.board[self.target].col == 'N':
@@ -12,7 +12,7 @@ class Checkmove:
             if self.target == self.pos + (8 + 1) or self.target == self.pos + (8 - 1):
                 return self.board[self.target].col == 'W'
         else: 
-            if self.pos >= 63 - 15 and self.pos <= 63 - 8:
+            if 63 - 15 <= self.pos <= 63 - 8:
                 if self.target == self.pos - (8) or self.target == self.pos - (8*2):
                     return self.board[self.target].col == 'N'
             elif self.target == self.pos - (8) and self.board[self.target].col == 'N':
@@ -79,31 +79,28 @@ class Checkmove:
         elif self.type == 'K': return self.__check_king()
         else: raise Exception('Unknown error in __check_piece')
 
-    def __check_castling():
-        pass
-
-    def __check_enpassant():
-        pass
-
-    def __check_promotion():
-        pass
-
-    def __check_meta(self): #stuff like castling, en passant, promotion
-        if self.__check_castling() or self.__check_enpassant() or self.__check_promotion():
-            return True
+    def __check_promotion(self):
+        if self.board[self.pos].move or not self.__check_pawn(): return False
+        if self.board[self.target].col == 'B':
+            if 56 <= self.board[self.target] <= 63: return True
         else: 
-            return False
+            if 0 <= self.board[self.target] <= 7: return True
+                
+
+    def __check_castling(self):
+        return False
+    
+    def __check_enpassant(self):
+        return False
 
     def check(self, startpos, endpos):
         self.pos = startpos
         self.target = endpos
         self.type = self.board[startpos].name
-        self.erow = self.target//8
-        self.srow = self.pos//8
-        self.ecol = self.target%8
-        self.scol = self.pos%8
-        self.dy = self.erow - self.srow
-        self.dx = self.ecol - self.scol
+        self.dy = self.target//8 - self.pos//8
+        self.dx = self.target%8 - self.pos%8
 
-        if self.__check_piece() or self.__check_meta(): return 1
-        else: return -1
+        if self.__check_promotion(): return "promotion"
+        if self.__check_castling(): return "castling"
+        if self.__check_enpassant(): return "enpassant"
+        return self.__check_piece()
