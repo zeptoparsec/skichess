f7093f9a7e9ed2e468972ef78a87c91b9cac6136
Kiwi-Co <111125019+Kiwi-Co@users.noreply.github.com>
2023-08-08 21:20:05 +0530

    Update boardstate.py

--- boardstate.py
+++ boardstate.py
@@ -102,14 +102,13 @@ class Boardstate:
         elif move_type == "castling":
             offset = 0 if self._board[startpos].col == 'B' else 56
             self.__move(startpos, endpos, move)
-            move = None
             if endpos == 1 + offset:
                 startpos = 0 + offset
                 endpos = 2 + offset
             elif endpos == 6 + offset:
                 startpos = 7 + offset
                 endpos = 5 + offset
-            self.__move(startpos, endpos, move)
+            self.__move(startpos, endpos, None)
             return 0
             
         self.__move(startpos, endpos, move)
