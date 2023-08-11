c55287038cba724c7b8fa6bce02c273a9029eaf9
Kiwi-Co <111125019+Kiwi-Co@users.noreply.github.com>
2023-08-11 15:26:07 +0530

    Add files via upload

--- interface.py
+++ interface.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 from boardstate import Boardstate
-from singleplayer import Singleplayer_menu
+from singleplayer import Singleplayer_pvp
 from multiplayer import Multiplayer_menu
 from os import system, name
 from time import sleep
@@ -19,27 +19,8 @@ parser.add_argument('-o',type=int,default=2)
 args = parser.parse_args()
 
 board = Boardstate()
-simpleplayer = Singleplayer_menu()
+singleplayer = Singleplayer_pvp([args.t,args.t],[args.i, args.i],True,args.legacy)
 multiplayer = Multiplayer_menu()
-
-x_axis = {'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5, 'f': 6, 'g': 7, 'h': 8}
-y_axis = {'8': 0, '7': 1, '6': 2, '5': 3, '4': 4, '3': 5, '2': 6, '1': 7}
-
-then = 0
-now = 0
-
-time = [args.t,args.t]
-inc = [args.i, args.i]
-
-turn = True
-legacy = args.legacy
-
-def makemove(move):
-    startpos = x_axis[move[0]] + y_axis[move[1]]*8 - 1
-    endpos = x_axis[move[2]] + y_axis[move[3]]*8 - 1
-
-    is_valid_input = (move[0: 3: 2].isalpha() and move[1: 4: 2].isnumeric()) == (len(move) == 4)
-    if not is_valid_input or board.makemove(startpos, endpos, turn, move, legacy) == -1: raise Exception
 
 def clearscreen():
     if name == 'nt': system('cls')
@@ -75,7 +56,7 @@ def menu():
 # 1 - Mulitplayer
 # 2 - Exit
 def page(option): 
-    if option == 0: pass
+    if option == 0: singleplayer.run()
     elif option == 1: pass
     elif option == 2: exit(0)
 
@@ -95,59 +76,3 @@ if args.o == 2:
         listener.join()
 else:
     page(args.o)
-
-while True:
-    clearscreen()
-    board.printboard(legacy)
-    time[int(not turn)] -= then - now - inc[int(not turn)]
-
-    print("Time spent:", then-now)
-    print("Time left :", time[int(not turn)])
-
-    if time[0] <= 0:
-        print('Black ran out of time!')
-        break
-    
-    if time[1] <= 0:
-        print('White ran out of time!')
-        break
-
-    now = int(datetime.now().timestamp())
-
-    if turn: move = input("White's turn: ").lower()
-    else: move = input("Black's turn: ").lower()
-    then = int(datetime.now().timestamp())
-
-    try:
-        if move == 'restart':
-            board.restart()
-            time = [600, 600]
-            turn = True
-            continue
-
-        if move == 'save':
-            clearscreen()
-            file = open(input('Enter the filename to save the game in: '), 'w')
-            file.write(board.getMoveHistory())
-            continue
-
-        if move == 'load':
-            clearscreen()
-            game = open(input('Enter the file to load the game from: '),'r').read()
-            turn = True
-
-            for i in game.split():
-                makemove(i)
-                turn = not turn
-
-            board.loadGame(game)
-            time = [600,600]
-            continue
-
-        makemove(move)
-    except:
-        print("Invalid move!")
-        sleep(1)
-    else:
-        turn = not turn
-
