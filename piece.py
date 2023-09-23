930719248e87a328b1d58ed9d9060cd60dace927
Kiwi-Co <111125019+Kiwi-Co@users.noreply.github.com>
2023-09-23 20:14:50 +0530

    Delete piece.py

--- piece.py
+++
@@ -1,9 +0,0 @@
-class Piece:
-    def __init__(self, name, pos, val, col, table):
-        self.name = name
-        self.pos = pos
-        self.val = val
-        self.col = col
-        self.table = table
-        self.moved = False
-        self.moved_again = False
