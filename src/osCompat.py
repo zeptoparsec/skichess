f879e1bad9424eee299d6550f40145b54a830346
kiwi-co <kevinbinoj@gmail.com>
2023-09-24 20:16:27 +0530

    Reduced scope of project, switched naming convention, fixed board/axis rotation input

---
+++ src/osCompat.py
@@ -0,0 +1,26 @@
+from os import name, system
+from itertools import chain
+
+def escapeFilePaths(path, file=True):
+    if name == 'nt':
+        ch = "\\"
+    else:
+        ch = "/"
+
+    xs = [ch]
+
+    for i in path[0:-1]:
+        xs.append(i)
+        xs.append(ch)
+
+    xs.append(path[-1].lower())
+    xs.append(ch)
+
+    if file:
+        xs.pop()
+
+    return ''.join(list(chain.from_iterable(xs)))
+
+def clr():
+    if name == 'nt': system('cls')
+    else: system('clear')
