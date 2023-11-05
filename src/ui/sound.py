0e70e583089a9d0aec38eeafb37f5f6e2d181269
zeptoparsec <qwern1177@outlook.com>
2023-11-05 11:09:28 +0530

    Migrate to pydub. (Currently doesn't work)

--- src/ui/sound.py
+++ src/ui/sound.py
@@ -4,7 +4,8 @@
     The sound subsystem.
 """
 
-from playsound import playsound
+from pydub import AudioSegment
+from pydub.playback import play
 from compat.osCompat import escapeFilePaths
 from engine.settings import settings
 from os import path, system
@@ -18,8 +19,8 @@ class Sound:
     def __init__(self):
         self.sound_path = path.dirname(path.abspath(__file__)) + escapeFilePaths(['..','..','data','sounds'], False)
 
-        self.menu_select = self.sound_path + 'menu_select.mp3'
-        self.menu_enter = self.sound_path + 'menu_enter.mp3'
+        self.menu_select = self.sound_path + 'menu_select.wav'
+        self.menu_enter = self.sound_path + 'menu_enter.wav'
 
         self.board_start_game = self.sound_path + 'board_start_game.mp3'
         self.board_move = self.sound_path + 'board_move.mp3'
@@ -27,7 +28,9 @@ class Sound:
 
     def __playSound(self, path):
         if settings.active_settings['sound']:
-            Thread(target=playsound, args=(path,)).start() # Need to use a different module to play sound
+            sound = AudioSegment.from_file(path, format="mp3")
+            play(sound)
+            #Thread(target=play, args=(sound)).start() # Need to use a different module to play sound
 
     def boardStartGame(self):
         self.__playSound(self.board_start_game)
